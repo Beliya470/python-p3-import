4a24506221da897313389545c5304d352b792ba9
Beliya470 <anneanziay78@gmail.com>
2023-09-07 17:23:30 +0300

    Completed Assignment

---
+++ lib/testing/import_test.py
@@ -0,0 +1,10 @@
+from absolute_package.package1 import module1, module2
+from absolute_package.package2 import module3, module4, module5
+from absolute_package.package2.subpackage1 import module6
+
+module1.function1()
+module2.function1()
+module3.function1()
+module4.function1()
+module5.function1()
+module6.function1()
